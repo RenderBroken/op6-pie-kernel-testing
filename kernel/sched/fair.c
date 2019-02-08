69fd7027fb1810a69c88b0a9ba18118d1de7643f
RenderBroken <zkennedy87@gmail.com>
2019-02-08 22:32:05 +0000

    Revert "sched: Use capacity_orig_of instead of capacity_of in overutilized function"
    This reverts commit 999b62ac7776177d1b3f29c0732797922da695bc.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6171,8 +6171,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 
 bool __cpu_overutilized(int cpu, int delta)
 {
-	return (capacity_orig_of(cpu) * 1024) <
-			((cpu_util(cpu) + delta) * capacity_margin);
+	return (capacity_of(cpu) * 1024) < ((cpu_util(cpu) + delta) * capacity_margin);
 }
 
 bool cpu_overutilized(int cpu)
