d6f5f0cc6533a9e71be92364ad706dad106bc56f
RenderBroken <zkennedy87@gmail.com>
2019-02-08 22:44:11 +0000

    Revert "sched: Make sure window start passed to schedutil is consistent"
    This reverts commit d8c5bfcc07ee88faed7ea276537356849b86b11d.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1893,7 +1893,7 @@ cpu_util_freq_walt(int cpu, struct sched_walt_cpu_load *walt_load)
 		walt_load->prev_window_util = util;
 		walt_load->nl = nl;
 		walt_load->pl = pl;
-		walt_load->ws = rq->load_reported_window;
+		walt_load->ws = rq->window_start;
 	}
 
 	return (util >= capacity) ? capacity : util;
@@ -2278,8 +2278,7 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags)
 		(rq->load_reported_window == rq->window_start) &&
 		!(flags & exception_flags))
 		return;
-	if (!(flags & exception_flags))
-		rq->load_reported_window = rq->window_start;
+	rq->load_reported_window = rq->window_start;
 #endif
 
 	data = rcu_dereference_sched(*per_cpu_ptr(&cpufreq_update_util_data,
