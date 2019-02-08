0603dc2168e889f95a80c6356ba1ccddcdd212d0
RenderBroken <zkennedy87@gmail.com>
2019-02-08 22:30:43 +0000

    Revert "sched/fair: Force balancing on NOHZ balance if local group has capacity"
    This reverts commit 3d9aec71e139bce6d592b56afaa30f02c344e80e.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9725,11 +9725,8 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 	if (busiest->group_type == group_imbalanced)
 		goto force_balance;
 
-	/*
-	 * When dst_cpu is idle, prevent SMP nice and/or asymmetric group
-	 * capacities from resulting in underutilization due to avg_load.
-	 */
-	if (env->idle != CPU_NOT_IDLE && group_has_capacity(env, local) &&
+	/* SD_BALANCE_NEWIDLE trumps SMP nice when underutilized */
+	if (env->idle == CPU_NEWLY_IDLE && group_has_capacity(env, local) &&
 	    busiest->group_no_capacity)
 		goto force_balance;
 
