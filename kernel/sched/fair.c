ce8ea084766bd5c4e462f39a38723fe34e17b3af
RenderBroken <zkennedy87@gmail.com>
2019-02-08 22:43:51 +0000

    Revert "sched/fair: Consider an idle CPU outside c-state as an active CPU"
    This reverts commit 646fe8f4a7c5ba170b82b8932842064fe6b0b873.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6833,15 +6833,11 @@ struct find_best_target_env {
 
 static bool is_packing_eligible(struct task_struct *p, int target_cpu,
 				struct find_best_target_env *fbt_env,
-				unsigned int target_cpus_count,
-				int best_idle_cstate)
+				unsigned int target_cpus_count)
 {
 	unsigned long tutil, estimated_capacity;
 
 	if (fbt_env->placement_boost || fbt_env->need_idle)
-		return false;
-
-	if (best_idle_cstate == -1)
 		return false;
 
 	if (target_cpus_count != 1)
@@ -7204,7 +7200,7 @@ retry:
 	} while (sg = sg->next, sg != sd->groups);
 
 	if (best_idle_cpu != -1 && !is_packing_eligible(p, target_cpu, fbt_env,
-					active_cpus_count, best_idle_cstate)) {
+					active_cpus_count)) {
 		if (target_cpu == task_cpu(p))
 			fbt_env->avoid_prev_cpu = true;
 
